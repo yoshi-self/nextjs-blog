feaa589218edd6b170c58417edba28b03fa09215
Yoshi <crispy245@gmail.com>
2021-04-08 16:45:46 +0900

    Done tutorial

---
+++ components/layout.js
@@ -0,0 +1,61 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from './layout.module.scss'
+import utilStyles from '../styles/utils.module.scss'
+import Link from 'next/link'
+
+const name = 'Yoshi'
+export const siteTitle = 'Yoshi.dev' 
+
+export default function Layout({ children, home }) {
+  return (
+    <div className={styles.container}>
+    <Head>
+      <link rel="icon" href="/favicon.io" />
+      <meta name="og:title" content={siteTitle} />
+      <meta name="twitter:card" content="summary_large_image" />
+    </Head>
+    <header className={styles.header}>
+      {home ? (
+        <>
+          <Image
+            src="/images/profile.jpg"
+            height={144}
+            width={144}
+            alt="something"
+          />
+          <h1 className={utilStyles.heading2Xl}>{name}</h1>
+        </>
+      ) : (
+        <>
+          <Link href="/">
+            <a>
+              <Image
+                priority
+                src="/images/profile.jpg"
+                className={utilStyles.borderCircle}
+                height={108}
+                width={108}
+                alt={name}
+              />
+            </a>
+          </Link>
+          <h2 className={utilStyles.headingLg}>
+            <Link href="/">
+              <a className={utilStyles.colorInherit}>{name}</a>
+            </Link>
+          </h2>
+        </>
+      )}
+    </header>
+    <main>{children}</main>
+    {!home && (
+      <div className={styles.backToHome}>
+        <Link href="/">
+          <a>← Back to home</a>
+        </Link>
+      </div>
+    )}
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,75 @@
 import Head from 'next/head'
+import Link from 'next/link'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.scss'
+import { getSortedPostData } from '../lib/posts'
+import Date from '../components/date'
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+    {typeof allPostsData}
+      <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+              {id}
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  )
+  /*
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Yoshi.dev</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Yoshi.dev
         </h1>
 
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Yoshi's website
         </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Image
+          src="/images/profile.jpg"
+          height={144}
+          width={144}
+          alt="Your Name"
+        />
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -206,4 +218,5 @@ export default function Home() {
       `}</style>
     </div>
   )
+  */
 }
